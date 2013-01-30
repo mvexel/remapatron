dfd8ab1b6d4147433744f6bdc07e37a570f98f5e
Martijn van Exel <mvexel@gmail.com>
2013-01-30 11:55:00 -0800

    refactor

--- scripts/maintenance_off.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mv /osm/software/remapping-microtask/webapp/index.html /osm/software/remapping-microtask/webapp/index-maintenance.html
-mv /osm/software/remapping-microtask/webapp/index0.html /osm/software/remapping-microtask/webapp/index.html
