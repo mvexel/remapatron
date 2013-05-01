8032e47b0366b4932244db69a03379422421c8f0
Martijn van Exel <m@rtijn.org>
2013-05-01 11:52:07 -0600

    geoenabling web service (legacy code)

--- service/get.py
+++ service/get.py
@@ -8,13 +8,14 @@ import logging
 CHALLENGE_ID = 10
 SKIP_THRESHOLD = 2 # number of times a task must have been marked before
 # it is no longer served up
-
+DISTANCE_THRESHOLD = 1 # degrees 
 logging.basicConfig(format='%(asctime)s %(message)s', filename='/var/log/maproulette/maproulette.log',level=logging.DEBUG)
 
 urls = (
     '/count/', 'getcount',
     '/store/(.*)/(-*\d+)', 'storeresult',
-    '/get/', 'getcandidate'
+    '/get/', 'getcandidate',
+    '/getgeo/(-*\d+\.*\d*)/(-*\d+\.*\d*)/(\d*\.*\d*)', 'getcandidategeo'
 )
 
 sys.stdout = sys.stderr
@@ -45,6 +46,42 @@ class getcandidate:
         cur2.close()
         cur.close()
         return geojson.dumps(out)
+
+class getcandidategeo:        
+    def GET(self, lon, lat, dist):
+        if not (lon and lat):
+            return web.badrequest()
+        lon = float(lon)
+        lat = float(lat)
+        if dist:
+            dist = float(dist)
+        else:
+            dist = DISTANCE_THRESHOLD
+        conn = psycopg2.connect("host=localhost dbname=osm user=osm password=osm")
+        cur = conn.cursor()
+        sql = """SELECT ST_AsGeoJSON(linestring), id  FROM 
+                mr_ways_no_lanes_challenge 
+                WHERE NOT done AND (type = 'motorway' or type = 'trunk') AND (current_timestamp - donetime) > '1 hour'
+                AND skipflag <= %s
+                AND linestring <-> ST_MakePoint(%f, %f) < %f
+                ORDER BY random LIMIT 1""" % (SKIP_THRESHOLD, lon, lat, dist)
+        logging.debug(sql)
+        cur.execute(sql)
+        recs = cur.fetchall()
+        if len(recs) == 0:
+            return web.notfound('no task near here')
+        (way,wayid) = recs[0]
+#lock the way
+        cur2 = conn.cursor()
+        sql = cur2.mogrify("SELECT mr_upsert(%s,%s)", (0,wayid))
+        logging.debug(sql)
+        cur2.execute("SELECT mr_upsert(%s,%s)", (0,wayid))
+        out = geojson.FeatureCollection([geojson.Feature(geometry=geojson.loads(way),properties={"id": wayid})])
+        conn.commit();
+        cur2.close()
+        cur.close()
+        return geojson.dumps(out)
+
 
 class storeresult:        
     def PUT(self,osmid,done):
